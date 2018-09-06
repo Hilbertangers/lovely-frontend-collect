51b5091e1b909383624360d7efd9eae614e4dda9
sfp <shufp@jjmmw.com>
2018-09-06 13:14:40 +0800

    change css 'sass'=>'styled-component'

--- src/components/ClickWave/index.js
+++ src/components/ClickWave/index.js
@@ -1,8 +1,100 @@
 import React, { Component } from 'react';
-import './index.css';
+import styled, { keyframes } from 'styled-components';
+
+const Wrapper = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-image: url('https://images.unsplash.com/photo-1440688807730-73e4e2169fb8?dpr=1&auto=format&fit=crop&w=1500&h=1001&q=80&cs=tinysrgb&crop=');
+    background-attachment: fixed;
+    background-position: center center; 
+    background-size: auto 100%;
+    overflow: hidden;
+    cursor: pointer;
+`;
+
+const WaveWrapper = styled.div`
+    position: absolute;
+    width: 80vmin;
+    height: 80vmin;
+    z-index: ${props => props.style.zIndex};
+    top: ${props => props.style.top};
+    left: ${props => props.style.left};
+`;
+
+const Wave = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+`;
+
+const Wave_frames = keyframes`
+        0% {
+            top: calc((100% - 10vmin)/2);
+            left: calc((100% - 10vmin)/2);
+            width: 10vmin;
+            height: 10vmin;
+            opacity: 1;
+        }
+        10% {
+            opacity: 1;
+        }
+        99% {
+            opacity: 1;
+        }
+        100% {
+            top: calc((100% - 40vmin)/2);
+            left: calc((100% - 40vmin)/2);
+            width: 40vmin;
+            height: 40vmin;
+            opacity: 0;
+        }
+`
+
+const WaveItem = styled.div`
+    position: absolute;
+    top: calc((100% - 20vmin)/2);
+    left: calc((100% - 20vmin)/2);
+    width: 20vmin;
+    height: 20vmin;
+    border-radius: 50%;
+    background-image: url('https://images.unsplash.com/photo-1440688807730-73e4e2169fb8?dpr=1&auto=format&fit=crop&w=1500&h=1001&q=80&cs=tinysrgb&crop=');
+    background-attachment: fixed;
+    background-position: center center;
+    transform: translate3d(0, 0, 0);
+    opacity: 0;
+    transition: all .2s;
+`
+
+const WaveItem1 = styled(WaveItem)`
+    background-size: auto 106%;
+    animation: ${Wave_frames} 1s ease-out .1s;
+    animation-fill-mode: forwards;
+    z-index: 10;
+`
+const WaveItem2 = styled(WaveItem)`
+    background-size: auto 102%;
+    animation: ${Wave_frames} 1s ease-out .15s;
+    animation-fill-mode: forwards;
+    z-index: 20;
+`
+const WaveItem3 = styled(WaveItem)`
+    background-size: auto 104%;
+    animation: ${Wave_frames} 1s ease-out .25s;
+    animation-fill-mode: forwards;
+    z-index: 30;
+`
+const WaveItem4 = styled(WaveItem)`
+    background-size: auto 100%;
+    animation: ${Wave_frames} 1s ease-out .4s;
+    animation-fill-mode: forwards;
+    z-index: 40;
+`
 
 class ClickWave extends Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
         let screenSizeWidth = document.body.clientWidth;
         let screenSizeHeight = document.body.clientHeight;
@@ -12,7 +104,7 @@ class ClickWave extends Component {
         };
         this.addWave = this.addWave.bind(this);
     }
-    addWave (e) {
+    addWave(e) {
         e.persist(); // 异步中想访问合成事件e, 需要调用此方法
         this.setState((prevState) => {
             prevState.waveList.push({
@@ -34,32 +126,31 @@ class ClickWave extends Component {
             }
         });
     }
-    waveListRender () {
-        return this.state.waveList.map((item, index) => {
-            if (item.alive > 0) {
-                return (<div 
-                    className="clickWave"
-                    key={index}
-                    style={{
-                        zIndex: index,
-                        top: item.pageY - this.halfvmin,
-                        left: item.pageX - this.halfvmin,
-                    }}>
-                    <div className="g-center">
-                        <div className="wave g-wave1"></div>
-                        <div className="wave g-wave2"></div>
-                        <div className="wave g-wave3"></div>
-                        <div className="wave g-wave4"></div>
-                    </div>
-                </div>)
-            } else return null;
-        });
-    }
-    render () {
+    render() {
         return (
-            <div className="clickWave-warpper" onClick={this.addWave}>
-                {this.waveListRender()}
-            </div>
+            <Wrapper onClick={this.addWave}>
+                {this.state.waveList.map((item, index) => {
+                    if (item.alive > 0) {
+                        return (
+                            <WaveWrapper
+                                key={index}
+                                style={{
+                                    zIndex: index,
+                                    top: item.pageY - this.halfvmin,
+                                    left: item.pageX - this.halfvmin,
+                                }}
+                                >
+                                <Wave>
+                                    <WaveItem1></WaveItem1>
+                                    <WaveItem2></WaveItem2>
+                                    <WaveItem3></WaveItem3>
+                                    <WaveItem4></WaveItem4>
+                                </Wave>
+                            </WaveWrapper>
+                        )
+                    } else return null
+                })}
+            </Wrapper>
         );
     }
 }
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import './index.css';
+import styled from 'styled-components';
+
+const StyleLink = styled(Link)`
+    display:block;
+`
 
 class home extends Component {
     render() {
         return (
             <div>
-                <Link className="block" to="/clickWave">点击水波图</Link>
-                <Link className="block" to="/waterWave">容器canvas水波图</Link>                    
+                <StyleLink className="block" to="/clickWave">点击水波图</StyleLink>
+                <StyleLink className="block" to="/waterWave">容器canvas水波图</StyleLink>
+                <StyleLink className="block" to="/piano">鼠标钢琴</StyleLink>
             </div>
         );
     }
